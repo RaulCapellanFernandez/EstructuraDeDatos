94d825b2532b244ea528f56d1e5acd92c64ad046
Cape <raulcape.7@gmail.com>
2019-04-05 15:33:36 +0200

    P4 a medias

--- edi-a004-2019/src/ule/edi/SimpleList/SingleLinkedListImpl.java
+++ edi-a004-2019/src/ule/edi/SimpleList/SingleLinkedListImpl.java
@@ -7,31 +7,42 @@ import java.util.NoSuchElementException;
 
 
 public class SingleLinkedListImpl<T> extends AbstractSingleLinkedListImpl<T> {
-	Node<T> first;
 	
 	public SingleLinkedListImpl(T ... elements) {
-		int longitud = elements.length;
-		first = new Node<T>(elements[0]);
+		header = null;
+		
+		if(elements.length > 0) {
+			Node<T> intro = new Node<T>(elements[0]);
+			header = intro;
+		}
 	
-		singleLinkedListImplRec(first, elements, longitud, 1);
+		singleLinkedListImplRec(header, elements, elements.length, 1);
 		
     }
 		
 	private void singleLinkedListImplRec(Node<T> head,T[] elementos, int longitud, int contador) {
-		System.out.println("Contador"+ contador+" Longitud"+ longitud);
-		
-		if(longitud != contador) {
+		if(longitud != contador && contador < longitud) {
 			Node<T> intro = new Node<T>(elementos[contador]);
 			head.next = intro; 
-			System.out.println("Head: "+head.content);
-			System.out.println("Head: "+head.next.content);
 			singleLinkedListImplRec(head.next, elementos, longitud, ++contador);
 		}
 	}
 
 	@Override
 	public void addLast(T element) {
-		
+		Node<T> intro = new Node<T>(element);
+		if(header == null) { 
+			header = intro;
+		}else
+			addLastRec(header, intro);
+			
+	}
+
+	private void addLastRec(Node<T> head, Node<T> intro) {
+		if(head.next != null)
+			addLastRec(head.next, intro);
+		else
+			head.next = intro;
 	}
 
 	@Override
@@ -42,19 +53,34 @@ public class SingleLinkedListImpl<T> extends AbstractSingleLinkedListImpl<T> {
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		if(header == null)
+			return true;
 		return false;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		 return sizeRec(header);
+	}
+
+	private int sizeRec(Node<T> head) {
+		if(head == null)
+			return 0;
+		else
+			return 1+sizeRec(head.next);
+		
 	}
 
 	@Override
 	public void addFirst(T element) {
-		// TODO Auto-generated method stub
+		Node<T> intro = new Node<T>(element);
+		if(header == null) 
+			header = intro;
+		else {
+			intro.next = header;
+			header = intro;
+		}
 		
 	}
 
@@ -62,26 +88,78 @@ public class SingleLinkedListImpl<T> extends AbstractSingleLinkedListImpl<T> {
 
 	@Override
 	public void addAtPos(T element, int p) {
-		// TODO Auto-generated method stub
-		
+		if(header == null || p == 1) 
+			addFirst(element);
+			
+		else if(p > size())
+			addLast(element);
+		else
+			addAtPosRec(header, element, p, 1);
+	}
+
+	private void addAtPosRec(Node<T> head, T element, int p, int num) {
+		Node<T> intro = new Node<T>(element);
+		if(p-1 != num)
+			addAtPosRec(head.next, element, p, ++num);
+		else {
+			intro.next = head.next;
+			head.next = intro;
+		}
 	}
 
 	@Override
 	public void addNTimes(T element, int n) {
-		// TODO Auto-generated method stub
+		addNTimesRec(element, n);
+	}
+
+	private void addNTimesRec(T element, int n) {
+		if(n > 0) {
+			addLast(element);
+			addNTimesRec(element, --n);
+		}
 		
 	}
 
 	@Override
 	public int indexOf(T elem) {
-		// TODO Auto-generated method stub
-		return 0;
+		return indexOfRec(header, elem);
+	}
+
+	private int indexOfRec(Node<T> head, T elem) {
+		if(head == null)
+			throw new NoSuchElementException();
+		if(head.content.equals(elem))
+			return 1;
+		else
+			return 1+indexOfRec(head.next, elem);
 	}
 
 	@Override
 	public T removeLast() throws EmptyCollectionException {
-		// TODO Auto-generated method stub
-		return null;
+		T sol;
+		if(header  == null)
+			throw new EmptyCollectionException("");
+		else if(size() == 1) {
+			Node<T> borrar = new Node<T>(header.content);
+			header = null;
+			return borrar.content;
+		}else if(size() == 2) {
+			Node<T> borrar = new Node<T>(header.next.content);
+			header.next = null;
+			return borrar.content;
+		}else
+			return sol  = removeLastRec(header);
+	}
+
+	private T removeLastRec(Node<T> head) {
+		if(head.next.next != null)
+			return removeLastRec(head.next);
+		else {
+			Node<T> borrar = new Node<T>(head.next.content);
+			head.next = null;
+			return borrar.content;
+		}
+			
 	}
 
 	@Override
--- edi-a004-2019/src/ule/edi/SimpleList/SingleLinkedListImplTests.java
+++ edi-a004-2019/src/ule/edi/SimpleList/SingleLinkedListImplTests.java
@@ -17,19 +17,83 @@ public class SingleLinkedListImplTests {
 
 	@Before
 	public void setUp() {
-		//this.lS = new SingleLinkedListImpl<String>();
-		
-		
+		this.lS = new SingleLinkedListImpl<String>();
 		this.lSABC = new SingleLinkedListImpl<String>("A", "B", "C");
-		System.out.println(lSABC.toString());
 	}
 
    @Test
    public void constructorElemens(){
-	   //lS=new SingleLinkedListImpl<String>("A", "B", "C", "D");
-	   Assert.assertEquals("[A, B, C, D]", lSABC.toString());
+	   lS=new SingleLinkedListImpl<String>("A", "B", "C", "D");
+	   Assert.assertEquals("[A, B, C, D]", lS.toString());
+	   Assert.assertEquals("[A, B, C]", lSABC.toString());
    }
-/*
+   @Test
+   public void isEmptyTest(){
+	   Assert.assertEquals(true, lS.isEmpty());
+	   Assert.assertEquals(false, lSABC.isEmpty());
+   }
+   @Test
+   public void sizeTest(){
+	   Assert.assertEquals(0, lS.size());
+	   Assert.assertEquals(3, lSABC.size());
+   }
+   @Test
+   public void addFirstTest(){
+	   lS.addFirst("1");
+	   Assert.assertEquals("[1]", lS.toString());
+	   lS.addFirst("2");
+	   lS.addFirst("3");
+	   Assert.assertEquals("[3, 2, 1]", lS.toString());
+   }
+   @Test
+   public void addLastTest(){
+	   lS.addLast("1");
+	   Assert.assertEquals("[1]", lS.toString());
+	   lS.addLast("2");
+	   lS.addLast("3");
+	   Assert.assertEquals("[1, 2, 3]", lS.toString());
+   }
+   @Test
+   public void addPosTest(){
+	   lS.addAtPos("1", 2);
+	   Assert.assertEquals("[1]", lS.toString());
+	   lS.addAtPos("2", 1);
+	   Assert.assertEquals("[2, 1]", lS.toString());
+	   lS.addAtPos("3", 24);
+	   Assert.assertEquals("[2, 1, 3]", lS.toString());
+	   lS.addAtPos("4", 3);
+	   Assert.assertEquals("[2, 1, 4, 3]", lS.toString());
+	   lS.addAtPos("5", 2);
+	   Assert.assertEquals("[2, 5, 1, 4, 3]", lS.toString());
+   }
+   @Test
+   public void addNTimesTest(){
+	   lS.addNTimes("5", 5);
+	   Assert.assertEquals("[5, 5, 5, 5, 5]", lS.toString());
+	   lSABC.addNTimes("5", 3);
+	   Assert.assertEquals("[A, B, C, 5, 5, 5]", lSABC.toString());
+	   
+   }
+   @Test
+   public void indexOfTest(){
+	   Assert.assertEquals(1, lSABC.indexOf("A"));
+	   Assert.assertEquals(2, lSABC.indexOf("B"));
+	   Assert.assertEquals(3, lSABC.indexOf("C"));
+   }
+   @Test
+   public void removeLastTest() throws EmptyCollectionException{
+	   Assert.assertEquals("C", lSABC.removeLast());
+	   Assert.assertEquals("[A, B]", lSABC.toString());
+	   Assert.assertEquals("B", lSABC.removeLast());
+	   Assert.assertEquals("[A]", lSABC.toString());
+	   Assert.assertEquals("A", lSABC.removeLast());
+	   Assert.assertEquals("[]", lSABC.toString());
+	   
+   }
+
+
+   
+
    @Test
    public void addAtPos(){
 	   
@@ -76,7 +140,5 @@ public class SingleLinkedListImplTests {
 			lS = new SingleLinkedListImpl<String>("A", "B", "C");
 			Assert.assertEquals(1, lS.isSubList(lSABC));	
 		}
-	 
-   */
 
 }
